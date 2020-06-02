8d64f45e9932e321cb4db7e8d9c73673a5f80627
admin <admin@MacBook-Pro-admin.local>
2020-06-02 06:40:56 +0300

    fix

--- supbackend/api/me/__init__.py
+++ supbackend/api/me/__init__.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import current_user
+from flask_jwt_extended import current_user, jwt_required
 from flask_smorest import Blueprint, abort
 
 from supbackend.api.auth.schema import MeSchema
@@ -8,6 +8,7 @@ blp = Blueprint("Me", __name__, url_prefix="/api/me")
 
 @blp.route("", methods=["GET"])
 @blp.response(MeSchema)
+@jwt_required
 def me():
     if current_user:
         return current_user
